c2b0af7e9752acd8bee3fbe510a1d85034f35e4b
Lareta <lareta064@gmail.com>
2020-11-07 16:59:36 +0300

    floating-btn

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-   
+   const bodyEl = document.body;
+   const previewBlock = document.getElementById('preview-content');
+   const floatBtn = document.getElementById('floating-button');
+  
+	// Определить праую координату плавающей кнопки
+   if(previewBlock){
+	   let floatBtnRightPosition = +((bodyEl.offsetWidth - previewBlock.offsetWidth) / 2 -20);
+	   floatBtn.style.right = floatBtnRightPosition +'px';
+
+   }
+
+   window.addEventListener('scroll', function(){
+	   if(window.pageYOffset > 100){
+		 if(previewBlock){
+			floatBtn.classList.add('active'); 
+			let floatBtnRightPosition = +((bodyEl.offsetWidth - previewBlock.offsetWidth) / 2 -20);
+			floatBtn.style.right = floatBtnRightPosition +'px';
+		} 
+		
+	   }else{
+			floatBtn.classList.remove('active'); 
+		} 
+   });
 })
